6c92c0cbc230fe0489b7f8396d6e3ee1356b1869
VLstack <github@vlstack.fr>
2025-09-21 11:45:37 +0200

    add docstrings

--- Sources/VLErrorKit/Description.swift
+++ Sources/VLErrorKit/Description.swift
@@ -3,11 +3,17 @@ import VLstackNamespace
 
 extension VLstack.DataError
 {
+ /// Represents a simple error description in VLErrorKit.
+ /// Conforms to Identifiable, Hashable, and Equatable.
  package struct Description: Identifiable, Hashable, Equatable
  {
+  /// Unique identifier for this description instance.
   package let id = UUID()
+
+  /// Text value of the error description.
   package let value: String
 
+  /// Hashes the description using its unique id and value.
   package func hash(into hasher: inout Hasher)
   {
    hasher.combine(id)
