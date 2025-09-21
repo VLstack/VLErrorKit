6f4035196d90ae2a627cc7e1eaeed468172db61a
VLstack <github@vlstack.fr>
2025-09-21 11:47:10 +0200

    add docstrings

--- Sources/VLErrorKit/protocol.swift
+++ Sources/VLErrorKit/protocol.swift
@@ -2,6 +2,8 @@ import VLstackNamespace
 
 extension VLstack.DataError
 {
+ /// Marker protocol for types that can be used as a context in VLstack.DataError.
+ /// Constrains conforming types to Hashable, Equatable, and Sendable.
  public protocol ContextType: Hashable, Equatable, Sendable
  {
  }
